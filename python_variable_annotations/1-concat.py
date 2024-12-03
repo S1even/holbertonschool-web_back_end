a99ffb34688379984c97270ffb8a9d7b37476120
S1even <8809@holbertonstudents.com>
2024-12-03 10:32:42 +0100

    Fix def

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -2,6 +2,6 @@
 """function that concatenates two strings"""
 
 
-def add(str1: str, str2: str) -> str:
+def concat(str1: str, str2: str) -> str:
     """returns a concatenated string"""
     return str1 + str2
