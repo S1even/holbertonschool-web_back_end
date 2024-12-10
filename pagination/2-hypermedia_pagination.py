1cf62209ec7a5f20f19b4c7020a1f5ab685980fa
S1even <8809@holbertonstudents.com>
2024-12-10 11:22:21 +0100

    Hypermedia pagination

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,54 +1,62 @@
 #!/usr/bin/env python3
 """function that implements a get_hyper method"""
-
-import math
-from typing import List, Dict, Any
 import csv
-
-
-index_range = __import__('0-simple_helper_function').index_range
+import math
+from typing import List, Dict
 
 
 class Server:
-    """Server class to paginate a dataset"""
+    """Server class to paginate a database of popular baby names"""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
     def __init__(self):
         self.__dataset = None
 
-        def dataset(self) -> List[List]:
-            """Cached dataset"""
-            if self.__dataset is None:
-                with open(self.DATA_FILE) as f:
-                    reader = csv.reader(f)
-                    dataset = [row for row in reader]
-                    self.__dataset = dataset[1:]
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
 
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List[Any]]:
-        """Returns a paginated dataset"""
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves a page of data from the dataset"""
+        assert isinstance(
+            page, int) and isinstance(
+            page_size, int), "Must be int"
+        assert page > 0 and page_size > 0, "The page and page_size must be > 0"
+        list = []
+        with open(self.DATA_FILE) as f:
+            read = csv.reader(f)
+            idxx = index_range(page, page_size)
+            for num, line in enumerate(read):
+                if num in range(idxx[0], idxx[1]):
+                    list.append(line)
+        return list
 
-        dataset = self.dataset()
-        start_index = (page - 1) * page_size
-        end_index = start_index + page_size
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Returns a dictionary with pagination metadata"""
+        dict = {}
+        idxx = index_range(page, page_size)
+        if (idxx[0] + 1) > len(self.dataset()):
+            dict["page_size"] = 0
+        else:
+            dict["page_size"] = idxx[1] - idxx[0]
+        dict["page"] = page
+        dict["data"] = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        dict["next_page"] = page + 1 if page < total_pages else None
+        dict["prev_page"] = page - 1 if page > 1 else None
+        dict["total_pages"] = total_pages
+        return dict
 
-        if start_index >= len(dataset):
-            return []
 
-        return dataset[start_index:end_index]
-
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
-        """Returns a dictionary with information pagination"""
-        data = self.get_page(page, page_size)
-        dataset_length = len(self.dataset())
-        total_pages = math.ceil(dataset_length / page_size)
-
-        return {
-            'page_size': len(data),
-            'page': page,
-            'data': data,
-            'next_page': page + 1 if page < total_pages else None,
-            'prev_page': page - 1 if page > 1 else None,
-            'total_pages': total_pages,
-        }
+def index_range(page, page_size):
+    """Calculate the start and end indices for a dataset page"""
+    baslangic = (page - 1) * page_size
+    bitis = page * page_size
+    return tuple([baslangic, bitis])
