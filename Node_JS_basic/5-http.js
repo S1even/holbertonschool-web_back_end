378dbc9d01e0b0b5e559f988a179c17f5ca6b1f9
S1even <Stevendevin95520@hotmail.com>
2025-01-20 14:23:23 +0100

    Create a more complex HTTP server using Node's HTTP module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,32 +1,27 @@
 const http = require('http');
-const url = require('url');
-
 const countStudents = require('./3-read_file_async');
 
-const path = process.argv[2];
-
 const app = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-
-  if (parsedUrl.pathname === '/') {
-    res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
     res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
-  } else if (parsedUrl.pathname === '/students') {
-    res.setHeader('Content-Type', 'text/plain');
+  } else if (req.url === '/students') {
     res.statusCode = 200;
-
+    res.setHeader('Content-Type', 'text/plain');
     res.write('This is the list of our students\n');
 
-    countStudents(path)
-      .then((output) => {
-        res.end(output);
+    const databasePath = process.argv[2];
+    countStudents(databasePath)
+      .then((result) => {
+        res.end(result);
       })
-      .catch((err) => {
-        res.end(err.message);
+      .catch((error) => {
+        res.end(error.message);
       });
   } else {
     res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
     res.end('Not Found');
   }
 });
