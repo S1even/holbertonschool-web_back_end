771ccc1f09b3da43536ff1700a3cdae62c15c99a
S1even <Stevendevin95520@hotmail.com>
2025-01-21 11:07:55 +0100

    Fix

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,67 +1,34 @@
 const http = require('http');
-const fs = require('fs').promises;
+const countStudents = require('./3-read_file_async');
 
-const databasePath = process.argv[2] || '';
-
-async function countStudents(path) {
-  try {
-    const data = await fs.readFile(path, 'utf8');
-
-    const lines = data.trim().split('\n');
-    const students = lines.slice(1).filter((line) => line);
-
-    const csStudents = [];
-    const sweStudents = [];
-
-    students.forEach((student) => {
-      const studentData = student.split(',');
-      const field = studentData[3];
-      if (field === 'CS') {
-        csStudents.push(studentData[0]);
-      }
-    });
-
-    students.forEach((student) => {
-      const studentData = student.split(',');
-      const field = studentData[3];
-      if (field === 'SWE') {
-        sweStudents.push(studentData[0]);
-      }
-    });
-
-    return {
-      totalStudents: students.length,
-      csStudents,
-      sweStudents,
-    };
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-}
+const port = 1245;
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    countStudents(databasePath)
-      .then((studentData) => {
-        const output = [
-          'This is the list of our students',
-          `Number of students: ${studentData.totalStudents}`,
-          `Number of students in CS: ${studentData.csStudents.length}. List: ${studentData.csStudents.join(', ')}`,
-          `Number of students in SWE: ${studentData.sweStudents.length}. List: ${studentData.sweStudents.join(', ')}`,
-        ].join('\n');
-        res.end(output);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    countStudents(process.argv[2])
+      .then((response) => {
+        res.end(`This is the list of our students\n${response}`);
       })
       .catch((error) => {
-        res.statusCode = 404;
+        res.statusCode = 500;
         res.end(error.message);
       });
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not found.');
   }
 });
 
-app.listen(1245);
+app.listen(port, () => {
+  console.log(`Server listen on port ${port}`);
+});
 
 module.exports = app;
