90639c1689b9fa533d8e6fbf0db4ef1630e48a49
S1even <Stevendevin95520@hotmail.com>
2026-03-06 10:21:03 +0100

    task 1

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Module for Session Authentication.
+"""
+import uuid
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    SessionAuth class that inherits from Auth.
+    """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for a user_id.
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+
+        session_id = str(uuid.uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
+    
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns a User ID based on a Session ID.
+        """
+        if session_id is None:
+            return None
+
+        if not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
