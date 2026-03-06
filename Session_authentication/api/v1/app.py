d25b128fa2e11214b1425c482f9fab7d2576d68b
S1even <Stevendevin95520@hotmail.com>
2026-03-06 10:37:16 +0100

    Before request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -36,7 +36,6 @@ def before_request() -> None:
     if auth is None:
         return
 
-    # Added /api/v1/auth_session/login/ to the excluded paths
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
@@ -47,7 +46,6 @@ def before_request() -> None:
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    # If both Authorization header AND Session cookie are missing, abort 401
     if auth.authorization_header(request) is None and \
        auth.session_cookie(request) is None:
         abort(401)
