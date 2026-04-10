b2f9af2f4b23890554643f76502e922bdfb41856
S1even <Stevendevin95520@hotmail.com>
2026-04-10 09:58:23 +0200

    Logout

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -42,3 +42,17 @@ def login() -> str:
     response.set_cookie(session_name, session_id)
 
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logout() -> str:
+    """ DELETE /api/v1/auth_session/logout
+    Handles the logout process for Session Authentication.
+    """
+    from api.v1.app import auth
+
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({}), 200
