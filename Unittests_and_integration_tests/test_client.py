ef84224f65daaf4b4015eb7929810d037806bd9b
S1even <Stevendevin95520@hotmail.com>
2026-04-22 10:55:57 +0200

    More patching

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,6 @@ Module containing unit tests for the client module.
 This module uses the unittest framework to test GithubOrgClient.
 """
 import unittest
-from unittest.mock import patch, Mock
 from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
@@ -57,3 +56,27 @@ class TestGithubOrgClient(unittest.TestCase):
             result = client._public_repos_url
 
             self.assertEqual(result, known_payload["repos_url"])
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json: Mock) -> None:
+        """
+        Tests the public_repos method to ensure it returns the correct list
+        of repositories and that mocked methods are called exactly once.
+        """
+        mock_payload = [{"name": "repo1"}, {"name": "repo2"}]
+        mock_get_json.return_value = mock_payload
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public_repos_url:
+
+            mock_url = "https://api.github.com/orgs/google/repos"
+            mock_public_repos_url.return_value = mock_url
+
+            client = GithubOrgClient("google")
+            result = client.public_repos()
+
+            self.assertEqual(result, ["repo1", "repo2"])
+
+            mock_public_repos_url.assert_called_once()
+
+            mock_get_json.assert_called_once_with(mock_url)
