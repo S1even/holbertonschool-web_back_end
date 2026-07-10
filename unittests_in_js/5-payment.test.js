43a8fdc82afcc2280d664113d89ef56ac98a66a1
S1even <Stevendevin95520@hotmail.com>
2026-07-10 10:26:54 +0200

    Hooks

---
+++ unittests_in_js/5-payment.test.js
@@ -0,0 +1,29 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./5-payment.js');
+
+describe('sendPaymentRequestToApi', function () {
+  let consoleSpy;
+
+  beforeEach(function () {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(function () {
+    consoleSpy.restore();
+  });
+
+  it('should log "The total is: 120" and be called once when called with 100 and 20', function () {
+    sendPaymentRequestToApi(100, 20);
+    
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+  });
+
+  it('should log "The total is: 20" and be called once when called with 10 and 10', function () {
+    sendPaymentRequestToApi(10, 10);
+    
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+  });
+});
